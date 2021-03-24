c44bfc4b15c3a9ad4a702e3b27382826090b48f3
shuangerwaiwai <1136949415@qq.com>
2021-03-24 16:05:49 +0800

    initila commit

---
+++ adv_gen.py
@@ -0,0 +1,141 @@
+from __future__  import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms, models
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+# epsilons = [0, .02, .04, .06, .08, .1, .2, .3]
+epsilons = [0, .0002, .0004, .0006, .0008, .001]
+
+pretrained_model = "my_model.pth"
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+data_dir = './FLAME_Data'
+save_adv_dir = './FLAME_Data/adv'
+
+input_size = 224
+
+batch_size = 1
+
+model_ft = models.resnet18(pretrained=False)
+num_ftrs = model_ft.fc.in_features
+model_ft.fc = nn.Linear(num_ftrs, 2)
+model_ft.to(device)
+model_ft.load_state_dict(torch.load(pretrained_model, map_location='cpu'))
+model_ft.eval()
+
+data_transforms = {
+    'test': transforms.Compose([
+        transforms.Resize(input_size),
+        transforms.CenterCrop(input_size),
+        transforms.ToTensor(),
+    ])
+}
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['test']}
+
+dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in ['test']}
+
+criterion = nn.CrossEntropyLoss()
+
+def fgsm_attack(image, eposilon, data_grad):
+    sign_data_grad = data_grad.sign()
+    perturbed_image = image + eposilon * sign_data_grad
+    perturbed_image = torch.clamp(perturbed_image, 0, 1)
+
+    return perturbed_image
+
+def get_adv(model, device, test_loader, epsilon, criterion):
+    
+    correct = 0,
+    correct = int(correct[0])
+    adv_examples = []
+
+    adv_image_count = 0,
+    adv_image_count = int(adv_image_count[0])
+
+    for data, target in test_loader['test']:
+        data, target = data.to(device), target.to(device)
+        data.requires_grad = True
+
+        outputs = model(data)
+
+        _, init_preds = torch.max(outputs, 1)
+        if init_preds != target.data:
+            continue
+
+        loss = criterion(outputs, target)
+        # loss = F.nll_loss(outputs, target)
+
+        model.zero_grad()
+
+        loss.backward()
+
+        data_grad = data.grad.data
+
+        perturbed_data = fgsm_attack(data, epsilon, data_grad)
+
+        output = model(perturbed_data)
+
+        _, final_pred = torch.max(output, 1)
+
+        if final_pred == target.data:
+            correct += 1
+            if (epsilon == 0) and (len(adv_examples) < 5):
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                data = data.squeeze().detach().cpu().numpy()
+                adv_examples.append((data, init_preds, final_pred, adv_ex))
+        else:
+            if len(adv_examples) < 5:
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                data = data.squeeze().detach().cpu().numpy()
+                adv_examples.append((data, init_preds, final_pred, adv_ex))
+
+            if epsilon == 0.0002:
+                perturbed_data = perturbed_data.squeeze()
+                perturbed_image = transforms.ToPILImage()(perturbed_data).convert('RGB')
+                if target.item() == 0:
+                    perturbed_image.save(os.path.join(save_adv_dir, 'Fire', 'adv' + str(adv_image_count) + '.png'))
+                else:
+                    perturbed_image.save(os.path.join(save_adv_dir, 'No_Fire', 'adv' + str(adv_image_count) + '.png'))
+                adv_image_count += 1
+
+    final_acc = correct/float(len(test_loader['test']))
+    print("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(epsilon, correct, len(test_loader['test']), final_acc))
+
+    return final_acc, adv_examples
+
+accuracies = []
+examples = []
+
+# Run test for each epsilon
+for eps in epsilons:
+    acc, ex = get_adv(model_ft, device, dataloaders_dict, eps, criterion)
+    accuracies.append(acc)
+    examples.append(ex)
+
+cnt = 0
+plt.figure(figsize=(50, 20))
+for i in range(len(epsilons)):
+    for j in range(len(examples[i])):
+        cnt += 1
+        plt.subplot(len(epsilons), len(examples[0]*2), cnt)
+        plt.xticks([], [])
+        plt.yticks([], [])
+        if j == 0:
+            plt.ylabel("Eps: {}".format(epsilons[i]), fontsize=14)
+        image,orig,adv,ex = examples[i][j]
+        plt.title("origin")
+        plt.imshow(image.transpose([1, 2, 0]))
+        cnt += 1
+        plt.subplot(len(epsilons), len(examples[0]*2), cnt)
+        plt.title("{} -> {}".format(orig, adv))
+        plt.imshow(ex.transpose([1, 2, 0]))
+plt.tight_layout()
+# plt.show()
+plt.savefig("someadv.png")
+
+
+
---
+++ main.py
@@ -0,0 +1,261 @@
+from __future__ import print_function
+from __future__ import division
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+#train：训练模式 test：测试模式
+#adv_train: 对抗训练 adv_test: 最后测试
+mode = 'ADV_TEST'
+
+#数据集的位置
+data_dir = './FLAME_Data'
+
+#使用的模型
+model_name = "resnet"
+
+#保存的最好的模型
+saved_model = 'my_model.pth'
+
+#对抗训练后保存的模型
+saved_adv_model = 'my_adv_model.pth'
+
+
+#最后的分类个数
+num_classes = 2
+
+#batch大小
+batch_size = 16
+
+#迭代的次数
+num_epochs = 10
+
+#True：特征提取  Flase：微调
+feature_extract = False
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def train_model(model, dataloaders, critertion, optimizer, num_epochs=25, is_inception=False):
+    #开始时间
+    since = time.time()
+
+    val_acc_history = []
+
+    #验证集上表现最好的模型
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs-1))
+        print('-' * 10)
+
+        if mode == 'TRAIN':
+            phases = ['train', 'val']
+        elif mode == 'ADV_TRAIN':
+            phases = ['adv_train', 'val']
+
+        for phase in phases:
+            if phase == 'train' or phase == 'adv_train':
+                model.train()
+            else:
+                model.eval()
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train' or phase == 'adv_train'):
+                    outputs = model(inputs)
+                    loss = critertion(outputs, labels)
+
+                    _, preds = torch.max(outputs, 1)
+
+                    if phase == 'train' or phase == 'adv_train':
+                        loss.backward()
+                        optimizer.step()
+
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # 保存最好的模型参数
+    if mode == 'TRAIN':
+        torch.save(best_model_wts, saved_model)
+    elif mode == 'ADV_TRAIN':
+        torch.save(best_model_wts, saved_adv_model)
+    model.load_state_dict(best_model_wts)
+    return model, val_acc_history
+
+def test_Model(model, dataloaders, critertion):
+    running_loss = 0.0
+    running_corrects = 0
+    for inputs, labels in dataloaders['test']:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        outputs = model(inputs)
+        loss = critertion(outputs, labels)
+        _, preds = torch.max(outputs, 1)
+
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data)
+
+    loss = running_loss / len(dataloaders['test'].dataset)
+    acc = running_corrects.double() / len(dataloaders['test'].dataset)
+
+    print('Loss: {:.4f} Acc: {:.4f}'.format(loss, acc))
+
+def set_parameters_requires_grad(model, feature_extracting):
+    #如果是微调，参数需要求导;如果是特称提取，则不需要求导
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+#初始化模型
+def initialize_model(model_name, num_classes, feature_extract, use_pretrained):
+    model_ft = None
+    input_size = 0
+
+    if model_name == "resnet":
+        if mode == 'TRAIN' or mode == 'ADV_TRAIN':
+            model_ft = models.resnet18(pretrained=use_pretrained)
+            set_parameters_requires_grad(model_ft, feature_extract)
+        elif mode == 'TEST' or mode == 'ADV_TEST':
+            model_ft = models.resnet18(pretrained=use_pretrained)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    return model_ft, input_size
+
+#损失函数
+criterion = nn.CrossEntropyLoss()
+
+
+# 需要更新的参数
+def params_to_update(model):
+    params_to_update = model.parameters()
+    print("Params to learn:")
+    if feature_extract:
+        params_to_update = []
+        for name, param in model.named_parameters():
+            if param.requires_grad == True:
+                params_to_update.append(param)
+                print("\t", name)
+    else:
+        for name, param in model.named_parameters():
+            if param.requires_grad == True:
+                print("\t", name)
+    return params_to_update
+
+#训练模式
+if mode == 'TRAIN':
+
+    model_ft, input_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=True)
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(input_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(input_size),
+            transforms.CenterCrop(input_size),
+            transforms.ToTensor(),
+        ])
+    }
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val']}
+
+    dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in ['train', 'val']}
+
+    model_ft = model_ft.to(device)
+
+    #需要更新的参数
+    params_to_update = params_to_update(model_ft)
+
+    #优化器
+    optimizer_ft = optim.SGD(params_to_update, lr=0.001, momentum=0.9)
+
+    model_ft, hist = train_model(model_ft, dataloaders_dict, criterion, optimizer_ft, num_epochs=num_epochs, is_inception=(model_name=="inception"))
+
+
+elif mode == 'TEST' or mode == 'ADV_TEST':
+    model_ft, input_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=False)
+    model_ft.to(device)
+    if mode == 'TEST':
+        model_ft.load_state_dict(torch.load(saved_model, map_location='cpu'))
+    elif mode == 'ADV_TEST':
+        model_ft.load_state_dict(torch.load(saved_adv_model, map_location='cpu'))
+    model_ft.eval()
+
+    data_transforms = {
+        'test': transforms.Compose([
+            transforms.Resize(input_size),
+            transforms.CenterCrop(input_size),
+            transforms.ToTensor(),
+        ])
+    }
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['test']}
+    dataloaders_dict = {
+        x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in ['test']}
+
+    test_Model(model_ft, dataloaders_dict, criterion)
+
+elif mode == 'ADV_TRAIN':
+    model_ft, input_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=False)
+    model_ft.to(device)
+    model_ft.load_state_dict(torch.load(saved_model, map_location='cpu'))
+    model_ft.eval()
+
+    data_transforms = {
+        'adv_train': transforms.Compose([
+            transforms.Resize(input_size),
+            transforms.CenterCrop(input_size),
+            transforms.ToTensor(),
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(input_size),
+            transforms.CenterCrop(input_size),
+            transforms.ToTensor(),
+        ])
+    }
+
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['adv_train', 'val']}
+    dataloaders_dict = {
+        x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in ['adv_train', 'val']}
+
+    params_to_update = params_to_update(model_ft)
+
+    # 优化器
+    optimizer_ft = optim.SGD(params_to_update, lr=0.001, momentum=0.9)
+
+    model_ft, hist = train_model(model_ft, dataloaders_dict, criterion, optimizer_ft, num_epochs=num_epochs,
+                                 is_inception=(model_name == "inception"))
